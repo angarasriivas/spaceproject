acab14ae293c00e88d910af8f05849f0c5b1f364
angarasriivas <163962024+angarasriivas@users.noreply.github.com>
2025-05-25 13:35:45 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,26 +1,4 @@
-// import React from 'react';
-// // import './sinup.css'; 
-// // import Myslider from './slider'; // Uncomment when using Myslider
-// // import Local from './local';
-// import "./App.css"
-// import Navbar from './roting/navbar';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// const App = () => {
-//   return (
-//     <div>
- 
-//       <Navbar/>
-//       <BrowserRouter>
-//       <Routes>
-//         <Route path='./first' Component={"firstpage"}></Route>
-//       </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// };
-
-// export default App;
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -33,16 +11,7 @@ import Project from './prject/project'
 const App = () => {
   return (
  <>
- {/* <Form/> */}
-  {/* <Navbar/>
-    <BrowserRouter>
- 
-      <Routes>
-        <Route path="./first" element={<Firstpage/>}/>
-        <Route path="./second" element={<Secondpage/>} />
-       
-      </Routes>
-    </BrowserRouter> */}
+
       <Project/>
     </>  
   
