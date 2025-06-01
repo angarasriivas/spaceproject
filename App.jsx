a4d5cc71f157761e35daef9011dc98ed445edede
angarasriivas <163962024+angarasriivas@users.noreply.github.com>
2025-06-01 16:07:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import TeamSection from './project.jsx/srinu ourteam';
+import ClientsReviews from './project.jsx/clientreview';
+import ContactUs from './project.jsx/contactus';
+function App() {
+  return (
+    <>
+   <TeamSection/>
+   <ClientsReviews/>
+   <ContactUs />
+
+    </>
+  );
+}
+
+export default App;
+
